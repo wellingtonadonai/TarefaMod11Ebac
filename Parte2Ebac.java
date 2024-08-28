8ff3bd3cc99a1a7bd3611325b71e98a534cd2b46
wellington <wellington.h.s.oliveira@hotmail.com>
2024-08-28 09:51:20 -0300

    Alteração e revisão do trabalho Ebac

--- Parte2Ebac.java
+++ Parte2Ebac.java
@@ -4,34 +4,59 @@ import java.util.List;
 
 public class Parte2Ebac {
 
+
     public static void main(String[] args) {
+        nomeHomemSeparado();
+        nomeMulherSeparado();
 
-        String nomes = " Wellington = H, Aline = M, Benedito = H, Carina = M ";
+    }
 
+    private static void nomeHomemSeparado() {
+        String nomesHomens = " Wellington = Homem, Benedito = Homem, Matheus = Homem, Tiago = Homem" ;
+        String[] nomeSeparadoHomens = nomesHomens.split(",");
 
+        List<String> Homens = new ArrayList<>();
 
-        String[] nomeSeparado = nomes.split(",");
-
-
-
-
-        List<String> separandoNomes = new ArrayList<>();
-
-
-        for (String nome : nomeSeparado ) {
-            separandoNomes.add(nome);
+        for (String nome : nomeSeparadoHomens ) {
+            Homens.add(nome);
         }
 
+        Collections.sort(Homens);
 
-
-        Collections.sort(separandoNomes);
-
-
-        for (String nome : separandoNomes) {
+        for (String nome : Homens) {
             System.out.println(nome);
+
+
+        }
+
+        System.out.println("");
+        System.out.println("********Lista de mulheres********** \n");
+    }
+
+
+    private static void nomeMulherSeparado() {
+
+        String nomesMulheres = " Aline = Mulher, Carina = Mulher, Catia = Mulher, Sara = Mulher ";
+        String[] nomeSeparadoM = nomesMulheres.split(",");
+
+        List<String> Mulheres = new ArrayList<>();
+
+
+        for (String nome : nomeSeparadoM ) {
+            Mulheres.add(nome);
+        }
+
+        Collections.sort(Mulheres);
+
+        for (String nome : Mulheres) {
+            System.out.println(nome);
+
+
         }
     }
+
 }
+
 
 
 
